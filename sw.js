4e01156f8e95d445f4e22eb4e474f7aabb6c2495
y947465-oss <y947465@gmail.com>
2025-08-22 09:31:52 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open('study-cache-v1').then(c=>c.addAll([
+    './','./index.html','./placement.html','./manifest.webmanifest',
+    './assets/icon-192.png','./assets/icon-512.png'
+  ])));
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
